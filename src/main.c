438251d130edf48800b52b0d307622448e10e25a
Hendrik Kahlen <hendrikkahlen.hk@gmail.com>
2017-06-17 22:13:34 +0200

    #2 VLC player changed to list.

--- src/main.c
+++ src/main.c
@@ -5,8 +5,9 @@
 int main() {
 	printf("Hello, World!\n");
 
-	playMovVideo("/home/icebreaker/Pictures/PossiblePictures/IMG_2235.MOV");
-	playMovVideo("/home/icebreaker/Pictures/PossiblePictures/IMG_2235.JPG");
+	playMovVideo("/home/icebreaker/Pictures/PossiblePictures");
+	//playMovVideo("/home/icebreaker/Pictures/PossiblePictures/IMG_2235.MOV");
+	//playMovVideo("/home/icebreaker/Pictures/PossiblePictures/IMG_2235.JPG");
 	//playMovVideo("/home/icebreaker/Pictures/PossiblePictures/Moderation.mp4");
 
 	return 0;
--- src/modules/MovPlayer.c
+++ src/modules/MovPlayer.c
@@ -9,12 +9,14 @@ bool playMovVideo(const char *pathToFile) {
 	libvlc_instance_t *inst;
 	libvlc_media_player_t *mp;
 	libvlc_media_t *m;
+	libvlc_media_list_player_t *media_list_player;
 
 	/* Load the VLC engine */
 	//preparation of the vlc command
-	const char * const vlc_args[] = {
-			"-V", "X11"
-			};
+	const char *const vlc_args[] = {
+			"-f", "-V", "X11"
+	};
+
 	inst = libvlc_new(sizeof(vlc_args) / sizeof(vlc_args[0]), vlc_args);
 
 	/* Create a new item */
@@ -24,17 +26,25 @@ bool playMovVideo(const char *pathToFile) {
 	/* Create a media player playing environement */
 	mp = libvlc_media_player_new_from_media(m);
 	libvlc_set_fullscreen(mp, 1);
+	media_list_player = libvlc_media_list_player_new(inst);
 
 	/* play the media_player */
 	libvlc_media_player_play(mp);
+	libvlc_media_list_player_set_playback_mode(media_list_player, libvlc_playback_mode_loop);
+	//printf("%i", libvlc_media_discoverer_is_running(libvlc_MediaDiscovererStarted));
+	libvlc_media_list_t* list = libvlc_media_list_new(inst);
+	libvlc_media_list_add_media(list, m);
+	libvlc_media_list_player_set_media_list(media_list_player,list);
 
-	sleep(3); /* Let it play a bit */
+	libvlc_media_list_player_play(media_list_player);
+
+	sleep(100); /* Let it play a bit */
 
 	/* Stop playing */
-	libvlc_media_player_stop(mp);
+	libvlc_media_list_player_stop(media_list_player);
 
 	/* Free the media_player */
-	libvlc_media_player_release(mp);
+	libvlc_media_list_player_release(media_list_player);
 
 	libvlc_release(inst);
 
