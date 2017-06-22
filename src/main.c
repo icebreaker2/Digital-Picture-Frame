eaf7b24f1dbcd231495edaeb547c526b1f0d1e1f
Hendrik Kahlen <hendrik.kahlen@uni-oldenburg.de>
2017-06-22 11:51:11 +0200

    README and main path finished. Example usage added.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 int main() {
 
 	uint32_t *length = calloc(sizeof(uint32_t), 1);
-	const char **fileList = getAllFilePathFromDir("/Your/Path/To/The/Media/Dir", &length);
+	const char **fileList = getAllFilePathFromDir("/Your/Path/To/Media/", &length);
 
 	playMultipleMediaFiles(fileList, *length);
 	return 0;
